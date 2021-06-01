95d13d608e6da8d3211112b4af9d5c2309b62fa3
rksharma1401 <rksharma1401@hotmail.com>
2021-06-01 15:13:21 +0530

    add rest

--- src/main/java/learn/demo/mongodb/rest/CustomerRest.java
+++ src/main/java/learn/demo/mongodb/rest/CustomerRest.java
@@ -1,8 +1,12 @@
 package learn.demo.mongodb.rest;
 
+import java.util.List;
 import java.util.Optional;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +25,12 @@ public class CustomerRest {
 	public Customer save(Customer cust) {
 		return repository.save(cust);
 	}
-	@PostMapping(path = "/")
-	public Optional<Customer> findById(String id) {
+	@PostMapping(path = "/{id}")
+	public Optional<Customer> findById(@PathParam("{id}") String id) {
 		return repository.findById(id);
+	}
+	@GetMapping(path ="/" )
+	public List<Customer> findAll() {
+		return repository.findAll();
 	}
 }
