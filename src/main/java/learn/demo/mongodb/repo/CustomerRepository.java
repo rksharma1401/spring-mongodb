cb7df7fa00d1dc3604ff763fd651ea461acf51b2
rksharma1401 <rksharma1401@hotmail.com>
2021-06-01 14:24:57 +0530

    first commit

---
+++ src/main/java/learn/demo/mongodb/repo/CustomerRepository.java
@@ -0,0 +1,15 @@
+package learn.demo.mongodb.repo;
+ 
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import learn.demo.mongodb.entity.Customer;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+  public Customer findByFirstName(String firstName);
+  public List<Customer> findByLastName(String lastName);
+
+}
